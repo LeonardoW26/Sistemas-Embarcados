373c64f3942543572e0d4482a6235a4a17661951
LeonardoW26 <110207338+LeonardoW26@users.noreply.github.com>
2023-05-31 22:06:09 -0300

    Desafio RTOS

--- main.cpp
+++ main.cpp
@@ -1,57 +1,98 @@
 #include <Arduino.h>
 #include <Arduino_FreeRTOS.h>
+#include <LiquidCrystal.h>
 
-#define LED_PIN_1 11
-#define LED_PIN_2 12
+#define LED_PIN_1 6
+#define LED_PIN_2 5
 
-//Prototipos de tarefas
-void TaskBlink1(void *pvParameters);
-void TaskBlink2(void *pvParameters);
+LiquidCrystal lcd(12,11,10,9,8,7);
 
-void setup (){
+// protótipos para as tarefas
+void TaskReadTemperature(void *pvParameters);
+void TaskLED(void *pvParameters);
+void TaskTemperatura(void *pvParameters);
 
-//inicializar os pinos dos LES como saidas
+// Variável para armazenar a temperatura lida
+volatile float temperature = 0.0;
+
+void setup() {
+ Serial.begin(9600);
+ lcd.begin(16,2);
+
 pinMode(LED_PIN_1, OUTPUT);
 pinMode(LED_PIN_2, OUTPUT);
 
-//cria as tarefas
+ // cria as tarefas
+ xTaskCreate(
+ TaskReadTemperature,
+ "ReadTemperature",
+ 128,
+ NULL,
+ 3,
+ NULL );
+//Crie as outras tarefas aqui
 xTaskCreate(
-    TaskBlink1, //função da tarefas - Ponteiro
-    "Blink1",
-    128, //tamanho da pilha
-    NULL, //Parametro da Tarefa
-    2, //prioridade - numeros mais altos - proporcional ao numero
-    NULL ); //agendar tarefa
+  TaskLED, //função da tarefas - Ponteiro
+    "LED",
+    128,
+    NULL,
+    2,
+    NULL );
 
-    xTaskCreate(
-    TaskBlink2, //função da tarefas - Ponteiro
-    "Blink2",
-    128, //tamanho da pilha
-    NULL, //Parametro da Tarefa
-    1, //prioridade - numeros mais altos - proporcional ao numero
-    NULL ); //agendar tarefa
+xTaskCreate(
+  TaskTemperatura, //função da tarefas - Ponteiro
+    "Temperatura",
+    128,
+    NULL,
+    1,
+    NULL );
+
+}
+void loop() {
+ // nada aqui!
 }
 
-void loop (){
-//nada aqui
-}
+void TaskReadTemperature(void *pvParameters) {
+ (void) pvParameters;
+ float sensorValue = 0.0;
+ for (;;) {
+ // Aqui você normalmente leria o valor do sensor de temperatura.
+ // Por simplicidade, vamos apenas simular um sensor variando a
+//temperatura de -10 a 40.
+ sensorValue = -10.0 + (rand() % 51); // gera um número aleatório entre
+//-10 e 40
+ temperature = sensorValue;
+ vTaskDelay(2000 / portTICK_PERIOD_MS); // aguarda por 2 segundos
+ lcd.setCursor(0,0);
+ lcd.print("Temp: ");
+ lcd.print(temperature);
+ lcd.print(" C");
 
-void TaskBlink1(void *pvParameters){
+
+ // vTaskDelay(1000 / portTICK_PERIOD_MS);
+ }
+}
+//Continue...
+
+void TaskLED(void *pvParameters){
     (void) pvParameters;
     for (;;){//loop infinito
         digitalWrite(LED_PIN_1, HIGH);
-        vTaskDelay(1000 / portTICK_PERIOD_MS); //
+        vTaskDelay(500 / portTICK_PERIOD_MS); //
         digitalWrite(LED_PIN_1, LOW);
-        vTaskDelay(1000 / portTICK_PERIOD_MS); //
+        vTaskDelay(500 / portTICK_PERIOD_MS); //
     }
 }
 
-void TaskBlink2(void *pvParameters){
+void TaskTemperatura(void *pvParameters){
     (void) pvParameters;
     for (;;){//loop infinito
+      if (temperature == 26){
+
         digitalWrite(LED_PIN_2, HIGH);
-        vTaskDelay(500 / portTICK_PERIOD_MS); //
+        vTaskDelay(5000 / portTICK_PERIOD_MS); //
         digitalWrite(LED_PIN_2, LOW);
-        vTaskDelay(500 / portTICK_PERIOD_MS); //
+        vTaskDelay(5000 / portTICK_PERIOD_MS); //
+      }  
     }
 }
